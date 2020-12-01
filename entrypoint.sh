733021768effca59444e70fef78186c3af9f7abe
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2020-12-02 08:25:19 +1100

    fix: Add catch clause for non-service account or broken service account implementations

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,15 @@ GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS:-/.google-crede
 
 set -euo pipefail
 
-gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
+if [[ -d $GOOGLE_APPLICATION_CREDENTIALS ]]; then
+    echo "$GOOGLE_APPLICATION_CREDENTIALS is a directory, skipping service-account login"
+elif [[ -f $GOOGLE_APPLICATION_CREDENTIALS ]]; then
+    echo "Found ${GOOGLE_APPLICATION_CREDENTIALS}, performing login."
+    gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
+else
+    echo "${GOOGLE_APPLICATION_CREDENTIALS} not found, using environment authentication."
+fi
+
 if [ ! -z "${GCP_KUBE_CLUSTER}" ]; then
     gcloud container clusters get-credentials "${GCP_KUBE_CLUSTER}" --region "$GCP_REGION" --project "${GCP_PROJECT}"
 fi
