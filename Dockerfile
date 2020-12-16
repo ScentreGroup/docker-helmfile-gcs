4286cbe81242941d32832fccd9d707753ff7d03f
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2020-12-16 12:23:45 +1100

    fix: Update gcloud, fix missing export for google credentials

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 ARG HELMFILE_VERSION=v0.116.0
 FROM quay.io/roboll/helmfile:helm3-${HELMFILE_VERSION}
-ARG CLOUD_SDK_VERSION=276.0.0
+ARG CLOUD_SDK_VERSION=317.0.0
 ARG HELM_GCS_VERSION=0.3.0
 ARG HELM_VERSION=v3.2.1
 ARG HELM_LOCATION="https://get.helm.sh"
@@ -13,7 +13,7 @@ RUN wget ${HELM_LOCATION}/${HELM_FILENAME} && \
 ENV CLOUD_SDK_VERSION=$CLOUD_SDK_VERSION
 ENV CLOUDSDK_PYTHON=python3
 ENV HELM_GCS_VERSION=${HELM_GCS_VERSION}
-ENV GOOGLE_APPLICATION_CREDENTIALS=/.google-credentials
+# ENV GOOGLE_APPLICATION_CREDENTIALS=/.google-credentials
 ENV PATH /google-cloud-sdk/bin:$PATH
 RUN apk add python3 && \
     curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${CLOUD_SDK_VERSION}-linux-x86_64.tar.gz && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,8 @@ helm version
 helmfile --version
 gcloud version
 
-GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS:-/.google-credentials}"
-
-set -euo pipefail
+export GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_APPLICATION_CREDENTIALS:-/.google-credentials}"
+export no_proxy="${no_proxy:-169.254.169.254,metadata,metadata.google.internal}"
 
 if [[ -d $GOOGLE_APPLICATION_CREDENTIALS ]]; then
     echo "$GOOGLE_APPLICATION_CREDENTIALS is a directory, skipping service-account login"
@@ -15,9 +14,13 @@ elif [[ -f $GOOGLE_APPLICATION_CREDENTIALS ]]; then
     gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
 else
     echo "${GOOGLE_APPLICATION_CREDENTIALS} not found, using environment authentication."
+    unset GOOGLE_APPLICATION_CREDENTIALS
 fi
 
+set -euo pipefail
+
 if [ ! -z "${GCP_KUBE_CLUSTER}" ]; then
+    gcloud auth list
     gcloud container clusters get-credentials "${GCP_KUBE_CLUSTER}" --region "$GCP_REGION" --project "${GCP_PROJECT}"
 fi
 
